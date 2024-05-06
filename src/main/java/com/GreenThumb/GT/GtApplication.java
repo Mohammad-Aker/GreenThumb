e5fee4ce4c303ba79aee24a2c3a9497b963ea258
mohammadaker <mohammadaker7@gmail.com>
2024-05-06 21:04:34 +0300

    Initial commit and file structure

---
+++ src/main/java/com/GreenThumb/GT/GtApplication.java
@@ -0,0 +1,13 @@
+package com.GreenThumb.GT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GreenThumb/GT/GtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GreenThumb.GT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
